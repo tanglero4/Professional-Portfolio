ec9126f730f2b32240de8e4316f161d666e0e627
tanglero4 <96814274+tanglero4@users.noreply.github.com>
2023-03-05 15:57:36 -0500

    Added footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './Components/navigation';
 import 'bulma/css/bulma.min.css';
+import Footer from './Components/footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <Navigation />
+      <Footer />
       </header>
 
     </div>
--- my-app/src/Components/footer.js
+++ my-app/src/Components/footer.js
@@ -1,1 +1,18 @@
 import React from 'react';
+
+
+function Footer() {
+    return (
+        <footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>Portfolio</strong> by <a href="https://github.com/tanglero4">Tiffany Anglero</a>. The source code is licensed
+      <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+      designed using <a href="https://bulma.io/">Bulma</a> and sourced on <a href='https://github.com/tanglero4/Professional-Portfolio'>Github</a>.
+    </p>
+  </div>
+</footer>
+    )
+   }
+  
+  export default Footer;
