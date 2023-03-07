4931a950f773816853a28f7ad539cda1a5dcdecd
tanglero4 <96814274+tanglero4@users.noreply.github.com>
2023-03-07 11:09:39 -0500

    Added main project code

---
+++ my-app/src/Components/Portfolio.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+function Portfolio() {
+  return (
+
+    <Row xs={1} md={2} className="g-4">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col>
+    <Card className="bg-dark text-white">
+      <Card.Img src="holder.js/100px270" alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Title>Project name</Card.Title>
+        <Card.Text>
+          What its about and programs used
+        </Card.Text>
+        <Card.Text>github logo and link</Card.Text>
+      </Card.ImgOverlay>
+    </Card>
+    </Col>))}
+    </Row>
+    );
+        }
+export default Portfolio;
+
+    // <Card className="bg-dark text-white">
+    //   <Card.Img src="holder.js/100px270" alt="Card image" />
+    //   <Card.ImgOverlay>
+    //     <Card.Title>Project name</Card.Title>
+    //     <Card.Text>
+    //       What its about and programs used
+    //     </Card.Text>
+    //     <Card.Text>github logo and link</Card.Text>
+    //   </Card.ImgOverlay>
+    // </Card>);
+    //     <Row xs={1} md={2} className="g-4">
+    //     {Array.from({ length: 4 }).map((_, idx) => (
+    //       <Col>
+    //         <Card>
+    //           <Card.Img variant="top" src="holder.js/100px160" />
+    //           <Card.Body>
+    //             <Card.Title>Card title</Card.Title>
+    //             <Card.Text>
+    //               This is a longer card with supporting text below as a natural
+    //               lead-in to additional content. This content is a little bit
+    //               longer.
+    //             </Card.Text>
+    //           </Card.Body>
+    //         </Card>
+    //       </Col>
+    //     ))}
+    //   </Row>
+    // );
