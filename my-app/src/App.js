495a28233af58d5417d06acdfd11c5546d1c651a
tanglero4 <96814274+tanglero4@users.noreply.github.com>
2023-03-10 20:40:34 -0500

    Updates

--- my-app/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import Container from './Components/Container'
-
-const App = () => <Container />;
-
-export default App;
--- my-app/src/Components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Header(){
-    return <div id='header'>
-
-    </div>
-}
-
-export default Header;
